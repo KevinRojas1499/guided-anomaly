b55d0d06eda3677aacea1ecc5e7fa22e4887821e
KevinRojas1499 <100324071+KevinRojas1499@users.noreply.github.com>
2024-03-28 15:51:33 -0400

    changing number of channels in unet

--- to_image.py
+++ to_image.py
@@ -2,13 +2,10 @@ import numpy as np
 # import wandb
 from PIL import Image
 
-images = np.load('samples_4_64x64x3.npz')
-print(images.files)
-print(images)
+images = np.load('images/samples_1x64x64x3.npz')
 # print(images['arr_0'])
 
 # print(images.shape())
 for i, imag in enumerate(images['arr_0']):
-    print(imag)
     im = Image.fromarray(imag)
     im.save(f'image {i}.png')
--- training_scripts/sample_no_guidance.sh
+++ training_scripts/sample_no_guidance.sh
@@ -1,6 +1,6 @@
 MODEL_FLAGS="--image_size 64 --num_channels 128 --num_res_blocks 2"
 CLASSIFIER_FLAGS="--image_size 64 --classifier_attention_resolutions 16,8 --classifier_depth 2 --classifier_width 128 --classifier_pool attention --classifier_resblock_updown True --classifier_use_scale_shift_norm True"
-SAMPLE_FLAGS="--batch_size 4 --num_samples 4 --timestep_respacing ddim25 --use_ddim True"
+SAMPLE_FLAGS="--batch_size 1 --num_samples 1 --timestep_respacing ddim25 --use_ddim True"
 mpiexec -n 1 python scripts/classifier_sample.py \
     --model_path checkpoints/score.pt \
     --classifier_path checkpoints/classifier.pt \
