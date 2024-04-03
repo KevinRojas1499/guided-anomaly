1ff3318f81e45f269e8a97d82b2e553c44b17460
Gabriela <grodrguez3@gatech.edu>
2024-04-03 14:36:08 -0400

    128

--- training_scripts/train_classifier.sh
+++ training_scripts/train_classifier.sh
@@ -1,5 +1,5 @@
 TRAIN_FLAGS="--iterations 10001 --anneal_lr True --batch_size 128 --lr 3e-4 --save_interval 100 --weight_decay 0.05"
-CLASSIFIER_FLAGS="--image_size 64 --classifier_attention_resolutions 16,8 --classifier_depth 2 --classifier_width 128 --classifier_pool attention --classifier_resblock_updown True --classifier_use_scale_shift_norm True"
+CLASSIFIER_FLAGS="--image_size 128 --classifier_attention_resolutions 16,8 --classifier_depth 2 --classifier_width 128 --classifier_pool attention --classifier_resblock_updown True --classifier_use_scale_shift_norm True"
 # export NCCL_P2P_DISABLE=1
-mpiexec -n 1 python scripts/classifier_train.py --data_dir OCT_DATA/processed/test/ $TRAIN_FLAGS $CLASSIFIER_FLAGS \
-    --log_dir /storage/home/hcoda1/7/krojas6/p-mtao8-0/repos/guided-anomaly/logging/classifier/
+mpiexec -n 1 python scripts/classifier_train.py --data_dir ./Preprocessed/train $TRAIN_FLAGS $CLASSIFIER_FLAGS \
+    --log_dir ./checkpoints
